f0c843d5e4cecdd93aa6769ab3183ce685de3120
Nickolas Xisto Machado <cabocanick@gmail.com>
2024-12-27 17:07:00 -0300

    funcionamento básico do fronto criado

--- pages/mainpage.py
+++ pages/mainpage.py
@@ -2,13 +2,17 @@
 import tkinter as tk
 from pages.advanced import AdvancedScreen
 from utils.navigationFunctions import switchPage
-
+from pages.resultPage import resultPage
 def mainPage(parent):
     mainFrame = tk.Frame(master= parent)
     w = tk.Label(mainFrame, text='Bem Vindo ao LoR Finder!')
+    inputLabel = tk.Label(mainFrame, text= "Busca por nome:")
     inp = tk.Entry(mainFrame)
+    searchbtn = tk.Button(mainFrame,  text= "Pesquisar", command=lambda:switchPage(mainFrame, resultPage, parent))
     btn = tk.Button(mainFrame, text="Busca Avançada", command=lambda: switchPage(mainFrame,AdvancedScreen, parent))
     w.pack()
     inp.pack()
+    searchbtn.pack()
+    inputLabel.pack()
     btn.pack()
     mainFrame.pack()
--- pages/resultPage.py
+++ pages/resultPage.py
@@ -3,8 +3,14 @@ from utils.resultCard import resultCard
 
 
 def resultPage(parent):
+    data = []
     resultFrame = tk.Frame(master=parent)
-    resultCard(parent)
+    if len(data) == 0:
+        noResult =tk.Label(resultFrame, text="Não há nenhum resultado!")
+        noResult.pack()
+    else:
+        for i in data:
+            resultCard(resultFrame, i)
 
 
 
