f4dbd8ec7464a7f6b80c924eeb532bf725a74fc6
Nickolas Xisto Machado <cabocanick@gmail.com>
2025-01-05 23:22:03 -0300

    yuumi jumpscare

--- pages/resultPage.py
+++ pages/resultPage.py
@@ -1,10 +1,47 @@
 import tkinter as tk
 from utils.resultCard import resultCard
+from CardStruct import Card
 
 
-
-def getSearchData():
-    data = []
+def getSearchData(): 
+    data = [Card( #alterar assim que a função de busca estiver pronta
+        name="Yuumi",
+        regions=["Bandle City", "Targon"],
+        cost=3,
+        attack=2,
+        health=1,
+        description_raw="Round Start: Grant the unit I'm Attached to +1|+1. Otherwise, grant me +1|+1 instead.", 
+        levelup_description_raw="I or the unit I'm Attached to have attacked 3 times.",
+        keywords=["Attatch"],
+        artist="SIXMOREVODKA",
+        spell_speed="",
+        game_absolute_path="http://dd.b.pvp.net/5_12_0/set5/en_us/img/cards/05BC029.png",
+        rarity="Champion",
+        expansion="Set5",
+        cardType="Unit",
+        subtypes=["FAE", "CAT"],
+        supertype="Champion",
+        flavor_text=""
+        ),
+     Card(
+        name="Fiora",
+        regions=["Demacia"],
+        cost=3,
+        attack=3,
+        health=3,
+        description_raw="When I'm on the board, slay 2 enemies to win the game.",
+        levelup_description_raw="I've seen you slay 4 enemies.",
+        keywords=["Challenger"],
+        artist="SIXMOREVODKA",
+        spell_speed="Fast",
+        game_absolute_path="http://dd.b.pvp.net/5_12_0/set5/en_us/img/cards/05BC029.png",
+        rarity="Champion",
+        expansion="Set 1",
+        cardType="Unit",
+        subtypes=["Elite"],
+        supertype="Champion",
+        flavor_text="Her strength is rivaled only by her discipline."
+    )]
     return data
 def resultPage(parent):
     data = getSearchData()
--- utils/resultCard.py
+++ utils/resultCard.py
@@ -1,16 +1,36 @@
 import tkinter as tk
 from utils.navigationFunctions import switchPage
 from pages.detailedPage import detailedCard
-from gera_arquivo_e_arvore import carta
-def resultCard(parent, data:carta):
+from CardStruct import Card
+import urllib.request
+from PIL import ImageTk, Image
+import io
 
+class WebImage:
+    def __init__(self, url):
+        with urllib.request.urlopen(url) as u:
+            raw_data = u.read()
+
+        image = Image.open(io.BytesIO(raw_data))
+        self.image = ImageTk.PhotoImage(image)
+
+    def get(self):
+        return self.image
+
+def resultCard(parent, data: Card):
     resultFrame = tk.Frame(master=parent)
-    nameLabel =tk.Label(resultFrame, text= data.name)
-    btn = tk.Button(resultFrame, text="detalhes", command=lambda: switchPage(resultFrame, detailedCard, parent))
 
-    
+    nameLabel = tk.Label(resultFrame, text=data.name)
+    img = WebImage(data.game_absolute_path).get()
+
+    # Store the image directly inside the label to prevent garbage collection
+    imgLabel = tk.Label(resultFrame, image=img)
+    imgLabel.image = img  # This ensures that the image is kept in memory
+
+    btn = tk.Button(resultFrame, text="detalhes", command=lambda: switchPage(resultFrame, detailedCard, parent))
 
     nameLabel.pack()
     btn.pack()
+    imgLabel.pack()
 
     resultFrame.pack()
