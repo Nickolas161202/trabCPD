b3f44a44e53cee1c9427c38b483e52b04afaa33b
Joel Soares Gonzalez <83096261+JoelSGonzalez@users.noreply.github.com>
2025-01-05 21:58:46 -0300

    Update CardStruct.py

--- CardStruct.py
+++ CardStruct.py
@@ -64,13 +64,24 @@ def exibir_pickle(nomeArquivo):
 
 def teste():
     Yuumi = Card(
-                "Yuumi",("Bandle City", "Targon"), 3, 2, 1,
-				 "Round Start: Grant the unit I'm Attached to +1|+1. Otherwise, grant me +1|+1 instead.", 
-                 "I or the unit I'm Attached to have attacked 3 times.",
-                 ("Attatch"), "SIXMOREVODKA", "",
-                 "http://dd.b.pvp.net/5_12_0/set5/en_us/img/cards/05BC029.png",
-                 "Champion", "Set5", "Unit", ("FAE", "CAT"), "Champion", ""
-                 )
+        name="Yuumi",
+        regions=["Bandle City", "Targon"],
+        cost=3,
+        attack=2,
+        health=1,
+        description_raw="Round Start: Grant the unit I'm Attached to +1|+1. Otherwise, grant me +1|+1 instead.", 
+        levelup_description_raw="I or the unit I'm Attached to have attacked 3 times.",
+        keywords=["Attatch"],
+        artist="SIXMOREVODKA",
+        spell_speed="",
+        game_absolute_path="http://dd.b.pvp.net/5_12_0/set5/en_us/img/cards/05BC029.png",
+        rarity="Champion",
+        expansion="Set5",
+        cardType="Unit",
+        subtypes=["FAE", "CAT"],
+        supertype="Champion",
+        flavor_text=""
+        )
     Fiora = Card(
         name="Fiora",
         regions=["Demacia"],
