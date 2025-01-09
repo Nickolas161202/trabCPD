a86a60aaeb13bb0a56bbe30a4816426638a246f4
Nickolas Xisto Machado <xistonickolas@gmail.com>
2025-01-09 17:33:43 -0300

    paginação criada

--- pages/resultPage.py
+++ pages/resultPage.py
@@ -4,48 +4,34 @@ from Classes import *
 from pages.detailedPage import detailedCard
 
 
-def getSearchData(): 
-    data = [Card( #alterar assim que a função de busca estiver pronta
-        name="Yuumi",
-        regions=["Bandle City", "Targon"],
-        cost=3,
-        attack=2,
-        health=1,
-        description_raw="Round Start: Grant the unit I'm Attached to +1|+1. Otherwise, grant me +1|+1 instead.", 
-        levelup_description_raw="I or the unit I'm Attached to have attacked 3 times.",
-        keywords=["Attatch"],
-        artist="SIXMOREVODKA",
-        spell_speed="",
-        game_absolute_path="http://dd.b.pvp.net/5_12_0/set5/en_us/img/cards/05BC029.png",
-        rarity="Champion",
-        expansion="Set5",
-        card_type="Unit",
-        subtypes=["FAE", "CAT"],
-        supertype="Champion",
-        flavor_text=""
-        ),
-     Card(
-        name="Fiora",
-        regions=["Demacia"],
-        cost=3,
-        attack=3,
-        health=3,
-        description_raw="When I'm on the board, slay 2 enemies to win the game.",
-        levelup_description_raw="I've seen you slay 4 enemies.",
-        keywords=["Challenger"],
-        artist="SIXMOREVODKA",
-        spell_speed="Fast",
-        game_absolute_path="http://dd.b.pvp.net/5_12_0/set5/en_us/img/cards/05BC029.png",
-        rarity="Champion",
-        expansion="Set 1",
-        card_type="Unit",
-        subtypes=["Elite"],
-        supertype="Champion",
-        flavor_text="Her strength is rivaled only by her discipline."
-    )]
-    return data
+
+def pagination(end, start, data, parent, grandParent):
+    for frames in parent.winfo_children():
+        frames.destroy()
+        
+    resultCard(parent, data[start], grandParent)
+    dtbtn = tk.Button(parent, text= "Detalhes", command= lambda: switchToDetail(grandParent, detailedCard, data[start]))
+    dtbtn.grid()
+    resultCard(parent, data[end], grandParent)
+    dtbtn1 = tk.Button(parent, text= "Detalhes", command= lambda: switchToDetail(grandParent, detailedCard, data[end]))
+    dtbtn1.grid()
+    prevEnd = end - 2
+    prevStart = start - 2
+    start = end+1 
+    end =  end +2
+    if end <= len(data):    
+        nextbtn = tk.Button(parent, text="next", command=lambda: pagination(end, start, data, parent, grandParent))
+        nextbtn.grid()
+        prevbtn = tk.Button(parent, text="prev", command=lambda: pagination(prevEnd, prevStart, data, parent, grandParent))
+        prevbtn.grid()
+    else:
+        
+        prevbtn = tk.Button(parent, text="prev", command=lambda: pagination(prevEnd, prevStart, data, parent, grandParent))
+        prevbtn.grid()
 
 
+    
+    
 def switchToDetail(parent, nextFrame, data):
     print(parent.winfo_children())
     for frames in parent.winfo_children():
@@ -53,7 +39,9 @@ def switchToDetail(parent, nextFrame, data):
     nextFrame(parent, data)
 
 def resultPage(parent, data:ColecaoDeCartas):
+   
     data = data.retorna_colecao()
+    print(data)
     resultFrame = tk.Frame(master=parent)
     resultFrame.grid(row=0, column=0, sticky="nsew")  
     
@@ -70,11 +58,15 @@ def resultPage(parent, data:ColecaoDeCartas):
     if len(data) == 0:
         noResult = tk.Label(secondFrame, text="Não há nenhum resultado!")
         noResult.pack()
-    else:
-        for item in  data: 
+    elif(len(data) <=2):
+        for item in data:
             resultCard(secondFrame, item, parent)
-            btn = tk.Button(secondFrame, text="detalhes", command=lambda:switchToDetail(parent, detailedCard, item))
-            btn.grid(column=1)  
+    else:
+
+         pagination(1, 0, data, secondFrame, parent)
+
+
+        
     
     resultFrame.grid_rowconfigure(0, weight=1)
     resultFrame.grid_columnconfigure(0, weight=1)
